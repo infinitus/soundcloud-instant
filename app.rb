541825f82015c868e7569d8f805d623783b45215
jin <jin@crypt.sg>
2014-12-01 03:21:17 +0800

    Search implemented with AJAX calls.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'soundcloud'
+require 'pp'
 
 if Sinatra::Base.development?
   require 'dotenv'
@@ -10,17 +11,23 @@ end
 CLIENT_ID = ENV['CLIENT_ID']
 
 get '/' do
-  @widget = SoundCloundClient.new(CLIENT_ID).track_html
+  client = SoundCloudClient.new(CLIENT_ID)
+  @widget = client.track_html
   erb :index
 end
 
-get '/search' do
-  @foo = params[:q]
-  erb :index
-  # @result = SoundCloundClient.new(CLIENT_ID).search
+post '/search' do
+  query = params[:q]
+
+  # TODO: DRY this.
+  client = SoundCloudClient.new(CLIENT_ID)
+  permalink = client.search_track(query)
+  p permalink
+  @widget = client.track_html(permalink)
+  erb :widget
 end
 
-class SoundCloundClient
+class SoundCloudClient
 
   attr_accessor :client
   DEFAULT_TRACK = "https://soundcloud.com/iamtchami/tchami-untrue-extended-mix"
@@ -29,10 +36,14 @@ class SoundCloundClient
     @client ||= SoundCloud.new(:client_id => CLIENT_ID)
   end
 
+  def search_track(query)
+    resp = client.get('/tracks', :q => query)
+    resp.first["permalink_url"]
+  end
+
   def track_html(track_url = DEFAULT_TRACK)
     resp = @client.get('/oembed', :url => track_url)
     resp["html"]
   end
 
 end
-
