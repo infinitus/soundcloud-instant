80fd3e98e4f73d2a820595fdafd09116c5bc8f9f
jin <jin@crypt.sg>
2014-12-02 01:13:55 +0800

    Wrapped app in a class

--- app.rb
+++ app.rb
@@ -2,40 +2,46 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'soundcloud'
 
-if Sinatra::Base.development?
-  require 'dotenv'
-  Dotenv.load
-  URL = ENV['DEVELOPMENT_URL']
-else
-  URL = ENV['PRODUCTION_URL']
-end
+class SoundCloudInstant < Sinatra::Application
 
-get '/' do
-  @widget = Client.widget
-  @url = URL
-  erb :index
-end
-
-post '/' do
-  query = params[:q]
-  @widget = Client.search_track(query)
-  response.headers['Access-Control-Allow-Origin'] = '*'
-  erb :widget
-end
-
-Client = SoundCloud.new(:client_id => ENV["CLIENT_ID"])
-class << Client
-
-  DEFAULT_TRACK = "https://soundcloud.com/iamtchami/tchami-untrue-extended-mix"
-
-  def search_track(query)
-    resp = self.get('/tracks', :q => query)
-    resp.first['uri']
+  if Sinatra::Base.development?
+    require 'dotenv'
+    Dotenv.load
+    URL = ENV['DEVELOPMENT_URL']
+  else
+    URL = ENV['PRODUCTION_URL']
   end
 
-  def widget(track_url = DEFAULT_TRACK)
-    resp = self.get('/oembed', :url => track_url)
-    resp["html"]
+  get '/' do
+    @widget = Client.widget
+    @url = URL
+    erb :index
+  end
+
+  post '/' do
+    query = params[:q]
+    @widget = Client.search_track(query)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    erb :widget
+  end
+
+  Client = SoundCloud.new(:client_id => ENV["CLIENT_ID"])
+  class << Client
+
+    DEFAULT_TRACK = "https://soundcloud.com/iamtchami/tchami-untrue-extended-mix"
+
+    def search_track(query)
+      resp = self.get('/tracks', :q => query)
+      resp.first['uri']
+    end
+
+    def widget(track_url = DEFAULT_TRACK)
+      resp = self.get('/oembed', :url => track_url)
+      resp["html"]
+    end
+
   end
 
 end
+
+SoundCloudInstant.run!
